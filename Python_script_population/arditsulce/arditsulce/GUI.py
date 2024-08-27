e5a3db8ce0e67c0401247eb7cfec05a3352a63f0
mrokas <rokas.michail@gmail.com>
2024-08-27 16:03:09 +0200

    add complete and exit button

--- Python_script_population/arditsulce/arditsulce/GUI.py
+++ Python_script_population/arditsulce/arditsulce/GUI.py
@@ -8,9 +8,13 @@ add_button = sg.Button('Add')
 
 list_box = sg.Listbox(values=functions.get_todos(), key ='todos', enable_events =True, size=[45,10])
 edit_button = sg.Button('Edit')
+complete_button = sg.Button('Complete')
+exit_button = sg.Button('Exit')
 
+
+layout = [[label], [input_box, add_button],[list_box,edit_button,complete_button],[exit_button]]
 window = sg.Window('My To-Do App',
-                       layout =[[label], [input_box, add_button],[list_box,edit_button]],
+                       layout =layout,
                        font = ('Helvetica',10)) # inside the tile of the GUI, definining the window
 while True:
     event , values = window.read() # to display the window
@@ -33,6 +37,15 @@ while True:
             window['todos'].update(values = todos) # this will give us the list box.'todos' points to the list box and update it with the todos values
         case 'todos':
             window['todo'].update(value = values['todos'][0]) #--> values is a list and we want to extract the string. with [0] we take the string as first element
+        case 'Complete':
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos) # here we update the listbox because it has key value 'todos'
+            window['todo'].update(value = '') # here we update input "add" because we use value = 'todo'
+        case 'Exit':
+            break
         case sg.WIN_CLOSED:
             break
 window.close() # to close the window
